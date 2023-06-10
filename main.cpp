b45fe6d88406ddd66e94bb82349281e396bf52ab
Dawid Kryński <128745052+antoni1988@users.noreply.github.com>
2023-06-10 23:49:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,64 @@
 #include <iostream>
+
 #include <vector>
+
 #include <stdexcept>
+
 #include <string>
+
 #include "class.h"
 
 using namespace std;
 int main(int argc, char * argv[]) {
-    
 
-    std::string inputString(argv[1]);
-    
-    //testowanie pliku
-    // /*
-    ASCIICanvas canvas(1, 1);
-    canvas.prepareFromFile(inputString);
-    canvas.draw();
-    // */
+  std::string inputString(argv[1]);
 
-    //testowanie ogolne
-    // /*
-    ASCIICanvas canvasTest(30, 30);
+  //testowanie pliku
+  // /*
+  ASCIICanvas canvas(1, 1);
+  canvas.prepareFromFile(inputString);
+  canvas.draw();
+  // */
 
-    canvasTest.setCharacter(19, 9, '@');
-    canvasTest.setCharacter(5, 3, '#');
-    canvasTest.setCharacter(7, 1, '*');
+  //testowanie ogolne
+  // /*
+  ASCIICanvas canvasTest(30, 30);
 
-    cout<<"Obraz:"<<endl;
-    canvasTest.draw();
-    cout<<"koniec obrazu"<<endl;
-    
-    canvasTest.setBackground('-');
-    
-    cout<<"Obraz:"<<endl;
-    canvasTest.draw();
-    cout<<"koniec obrazu"<<endl;
-    
-    Triangle triangle(1, 1,'a', 10,7);
-    Circle circle(7,15,'#',6);
-    Rectangle rectangle(8, 1,'b', 6,3);
-    Square square(14, 6,'d', 4);
-    
-    cout<<"Obraz:"<<endl;
-    triangle.draw(canvasTest);
-    canvasTest.draw();
-    cout<<"koniec obrazu"<<endl;
-    
-    cout<<"Obraz:"<<endl;
-    rectangle.draw(canvasTest);
-    canvasTest.draw();
-    cout<<"koniec obrazu"<<endl;
-    
-    cout<<"Obraz:"<<endl;
-    circle.draw(canvasTest);
-    canvasTest.draw();
-    cout<<"koniec obrazu"<<endl;
-    //*/
-    canvasTest.exportToFile("plik.txt");
-    
+  canvasTest.setCharacter(19, 9, '@');
+  canvasTest.setCharacter(5, 3, '#');
+  canvasTest.setCharacter(7, 1, '*');
 
-    return 0;
+  cout << "Obraz:" << endl;
+  canvasTest.draw();
+  cout << "koniec obrazu" << endl;
+
+  canvasTest.setBackground('-');
+
+  cout << "Obraz:" << endl;
+  canvasTest.draw();
+  cout << "koniec obrazu" << endl;
+
+  Triangle triangle(1, 1, 'a', 10, 7);
+  Circle circle(7, 15, '#', 6);
+  Rectangle rectangle(8, 1, 'b', 6, 3);
+  Square square(14, 6, 'd', 4);
+
+  cout << "Obraz:" << endl;
+  triangle.draw(canvasTest);
+  canvasTest.draw();
+  cout << "koniec obrazu" << endl;
+
+  cout << "Obraz:" << endl;
+  rectangle.draw(canvasTest);
+  canvasTest.draw();
+  cout << "koniec obrazu" << endl;
+
+  cout << "Obraz:" << endl;
+  circle.draw(canvasTest);
+  canvasTest.draw();
+  cout << "koniec obrazu" << endl;
+  //*/
+  canvasTest.exportToFile("plik.txt");
+
+  return 0;
 }
